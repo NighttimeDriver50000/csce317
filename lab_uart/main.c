26ee6b467704fe2ff72b1e186dab41d6ba813ad8
Chris McKinney <chris@tachibanatech.com>
2020-01-31 15:37:06 -0500

    example led flashing

--- lab_uart/main.c
+++ lab_uart/main.c
@@ -1,21 +1,67 @@
+/* library for interacting with AVR interrupts */
+
+#include <avr/interrupt.h>
+
+
+/* AVR I/O pin definitions */
+
 #include <avr/io.h>
-#include <stdio.h>
+
+
+/* standard C library functions */
+
 #include <stdlib.h>
+
+
+/* string maniuplation functions */
+
 #include <string.h>
+
+
+/* CPU speed-adjusted delay utilities */
+
 #include <util/delay.h>
 
 
 int main(void) {
-	/* initialize PC0 as an output */
 
-	/* while true ... */
 
-		/* sleep for 1/2 second */
+    /* Place your code here. This is just a simple LED blink program. that
 
-		/* set PC0 high */
+     *  will make PC1 blink every 100ms */
 
-		/* sleep for 1/2 second */
 
-		/* set PC0 to low */
+    /* Set all pins of port C as an output. We could set just PC1 as an
+
+     * output with DDRC = 0x02 (binary 00000010) if we wanted to */
+
+    DDRC = 0xFF;
+
+
+    /* loop forever */
+
+    while(1) {
+
+
+        /* Turn on all pins on port C. We could turn off just PC1
+
+         * using PORTC |= 0x0 */
+
+        PORTC = 0xFF;
+
+
+        _delay_ms(100);
+
+
+        /* Turn off all pins on port C. We could turn off just PC1
+
+         * using PORTC &= ~(0x02) */
+
+        PORTC = 0x00;
+
+
+        _delay_ms(100);
+
+    }
+
 }
-
