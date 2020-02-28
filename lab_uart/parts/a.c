bd783366e253701b92ea37449d28480a16b6013b
Michael <ms.321@aol.com>
2020-02-28 15:11:38 -0500

    added comments to a.c

--- lab_uart/parts/a.c
+++ lab_uart/parts/a.c
@@ -1,7 +1,20 @@
-#include <stdint.h>
+include <stdint.h>
 #include <avr/io.h>
 #include <util/delay.h>
 
+/*static inline void creates a function which will allow the compiler to do inling which
+  typically requires optimization to be enabled.
+  exp -
+  static inline void swap(int *m, int *n)
+   {
+    int tmp = *m;
+    *m = *n;
+    *n = tmp;
+   }
+   along with this we have uint8_t which a byte, shorthand for unsigned integer of length
+   8 bits.
+
+*/
 static inline void setBitByIndex(volatile uint8_t* reg, uint8_t index, uint8_t value) {
     if (value) {
         *reg |= (1 << index);
@@ -10,8 +23,13 @@ static inline void setBitByIndex(volatile uint8_t* reg, uint8_t index, uint8_t v
     }
 }
 
+//main method
 int main(void) {
-    setBitByIndex(&DDRC, DDC0, 1);
+
+	//function call for setBit by index where register is the DataDirection Register DDRD
+	//the index is 
+	setBitByIndex(&DDRC, DDC0, 1);
+	//while loop for moving though the 
     while (1) {
         setBitByIndex(&PORTC, PORTC0, 0);
         _delay_ms(500);
