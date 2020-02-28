cb95ce0979b0762068814429c05623c9fa3a5c98
Emberlynn McKinney <ntd5@tachibanatech.com>
2020-02-28 16:10:54 -0500

    copied over previous part c

--- lab_lis3dh_spi/main.c
+++ lab_lis3dh_spi/main.c
@@ -1,10 +1,150 @@
-#include <avr/io.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <avr/interrupt.h>
+#include <avr/io.h>
 #include <util/delay.h>
 
-
-int main(void) {
+void setBitByIndex(volatile uint8_t* reg, uint8_t index, uint8_t value) {
+    if (value) {
+        *reg |= (1 << index);
+    } else {
+        *reg &= ~(1 << index);
+    }
 }
 
+void setMultiBitByIndex(volatile uint8_t* reg, uint8_t pairs, ...) {
+    va_list args;
+    va_start(args, pairs);
+    for (uint8_t i = 0; i < pairs; ++i) {
+        uint8_t index = (uint8_t)va_arg(args, int);
+        uint8_t value = (uint8_t)va_arg(args, int);
+        setBitByIndex(reg, index, value);
+    }
+    va_end(args);
+}
+
+int uart_putchar(char c, FILE *stream) {
+    loop_until_bit_is_set(UCSR0A, UDRE0);
+    if (c == '\n') {
+        uart_putchar('\r', stream);
+    }
+    loop_until_bit_is_set(UCSR0A, UDRE0);
+    UDR0 = c;
+    return 0;
+}
+
+int uart_getchar(FILE *stream) {
+    loop_until_bit_is_set(UCSR0A, RXC0);
+    return UDR0;
+}
+
+void uart_init(void) {
+    UBRR0 = (F_CPU / (UART_BAUDRATE * 16UL)) - 1;
+    setMultiBitByIndex(&UCSR0B, 2, TXEN0, 1, RXEN0, 1);
+
+    setMultiBitByIndex(&UCSR0C, 5,
+            UCSZ01, 1, UCSZ00, 1, // character size 8
+            USBS0, 0, // 1 stop bit
+            UPM01, 0, UPM00, 0); // no parity
+
+    fdevopen(uart_putchar, NULL);
+    fdevopen(NULL, uart_getchar);
+
+    printf("\n\nUART initialized (%i 8N1)\n", UART_BAUDRATE);
+}
+
+void spi_init(void) {
+    setMultiBitByIndex(&SPCR, 8,
+            SPIE, 0, // no SPI interrupt
+            SPE, 1, // enable SPI
+            DORD, 0, // MSB first
+            MSTR, 1, // master mode
+            CPOL, 0, // leading rising, trailing falling
+            CPHA, 0, // leading sample, trailing setup
+            SPR1, 1, SPR0, 1); // 128 clock divider
+}
+
+#define SS   PORTB2
+#define MOSI PORTB3
+#define MISO PORTB4
+#define SCLK PORTB5
+
+void lis3dh_init(void) {
+}
+
+volatile uint8_t overflows = 0;
+
+#define DIVIDER (256)
+#define INTERVAL ((uint8_t)((F_CPU / DIVIDER) / (1 << 8)))
+
+ISR(TIMER0_OVF_vect) {
+    ++overflows;
+    if (overflows == INTERVAL / 2) {
+        setBitByIndex(&PORTC, PORTC0, 0);
+    } else if (overflows >= INTERVAL) {
+        setBitByIndex(&PORTC, PORTC0, 1);
+        overflows = 0;
+    }
+}
+
+#define MAX_COMMAND_LENGTH (64)
+
+char command[MAX_COMMAND_LENGTH];
+uint8_t commandLength = 0;
+
+uint8_t matchesCommand(char* compare) {
+    for (uint8_t i = 0; i < commandLength; ++i) {
+        if (compare[i] == '\0' || compare[i] != command[i]) {
+            return 0;
+        }
+    }
+    return (commandLength == MAX_COMMAND_LENGTH
+            || compare[commandLength] == '\0');
+}
+
+void resetPrompt() {
+    commandLength = 0;
+    printf("\nready> ");
+    fflush(stdout);
+}
+
+int main(void) {
+    uart_init();
+    setBitByIndex(&DDRC, DDC0, 1);
+    setBitByIndex(&PORTC, PORTC0, 0);
+    cli();
+    setMultiBitByIndex(&TCCR0B, 3, CS02, 1, CS01, 0, CS00, 0);
+    setBitByIndex(&TIMSK0, TOIE0, 1);
+    resetPrompt();
+    int c;
+    while (1) {
+        c = uart_getchar(stdin);
+        if (c == '\r' || c == '\n') {
+            if (commandLength == 0) {
+                // pass
+            } else if (matchesCommand("on")) {
+                overflows = 0;
+                TCNT0 = 0;
+                setBitByIndex(&PORTC, PORTC0, 1);
+                sei();
+            } else if (matchesCommand("off")) {
+                cli();
+                setBitByIndex(&PORTC, PORTC0, 0);
+            } else {
+                printf("\nUnknown command. Valid commands: on, off");
+            }
+            resetPrompt();
+        } else if (c == '\b' || c == '\x7f') {
+            if (commandLength != 0) {
+                --commandLength;
+            }
+        } else if (commandLength == MAX_COMMAND_LENGTH) {
+            printf("\nMaximum command length %d exceeded", MAX_COMMAND_LENGTH);
+            resetPrompt();
+        } else {
+            command[commandLength] = c;
+            ++commandLength;
+        }
+    }
+}
