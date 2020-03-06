9cdf892ad626036d6ce54aec9ca58dea8428539c
Emberlynn McKinney <ntd5@tachibanatech.com>
2020-03-05 19:19:24 -0500

    Working Part A, I believe

--- lab_lis3dh_spi/main.c
+++ lab_lis3dh_spi/main.c
@@ -83,7 +83,6 @@ volatile uint8_t spi_batch_count = 0;
 
 // NOTE: These have no overflow/underflow protections. Use at own risk.
 void push_spi_batch() {
-    cli();
     ++spi_batch_count;
     setBitByIndex(&PORTB, SS, 0);
 }
@@ -91,7 +90,6 @@ void pop_spi_batch() {
     --spi_batch_count;
     if (spi_batch_count == 0) {
         setBitByIndex(&PORTB, SS, 1);
-        sei();
     }
 }
 
@@ -121,34 +119,76 @@ void lis3dh_write(uint8_t reg, uint8_t value) {
     pop_spi_batch();
 }
 
-void lis3dh_setup(void) {
-    lis3dh_write(CTRL_REG1, 0x17); // 1Hz, normal mode, X/Y/Z all enabled
-    lis3dh_write(CTRL_REG5, 0x08); // No FIFO, latch interrupts
-    lis3dh_write(FIFO_CTRL_REG, 0x00); // No FIFO, so Bypass
-    lis3dh_write(CTRL_REG3, 0x10); // Enable DRDY1 interrupt on INT1
-    lis3dh_write(CTRL_REG6, 0x02); // Active low INT1
-    setMultiBitByIndex(&EICRA, 2, ISC01, 0, ISC00, 0);
-    setBitByIndex(&EIMSK, INT0, 1);
+int16_t bytes2short(uint8_t lo, uint8_t hi) {
+    uint16_t hi16 = hi;
+    uint16_t raw = (hi16 << 8) | lo;
+    return *(int16_t*)(&raw);
 }
 
-/////// LIS3DH DATA READY INTERRUPT HANDLER ///////////////////////////////////
+volatile int16_t x = 0;
+volatile int16_t y = 0;
+volatile int16_t z = 0;
 
-volatile uint16_t x = 0;
-volatile uint16_t y = 0;
-volatile uint16_t z = 0;
-
-ISR(INT0_vect) {
+void lis3dh_update_xyz() {
     uint8_t xl = lis3dh_read(OUT_X_L);
     uint8_t xh = lis3dh_read(OUT_X_H);
     uint8_t yl = lis3dh_read(OUT_Y_L);
     uint8_t yh = lis3dh_read(OUT_Y_H);
     uint8_t zl = lis3dh_read(OUT_Z_L);
     uint8_t zh = lis3dh_read(OUT_Z_H);
-    x = (xh << 8) | xl;
-    y = (yh << 8) | yl;
-    z = (zh << 8) | zl;
-    printf("XYZ: (0x%04x, 0x%04x, 0x%04x)\n", x, y, z);
-    printf("INT1_SOURCE: 0x%02x\n", lis3dh_read(INT1_SOURCE));
+    x = bytes2short(xl, xh);
+    y = bytes2short(yl, yh);
+    z = bytes2short(zl, zh);
+}
+
+//#define POLLING
+
+void lis3dh_setup(void) {
+    cli();
+    lis3dh_write(CTRL_REG1, 0x27); // 25Hz, normal mode, X/Y/Z all enabled
+    lis3dh_write(CTRL_REG5, 0x08); // No FIFO, latch interrupts
+    lis3dh_write(FIFO_CTRL_REG, 0x00); // No FIFO, so Bypass
+    lis3dh_write(CTRL_REG3, 0x10); // Enable DRDY1 interrupt on INT1
+    lis3dh_write(CTRL_REG6, 0x02); // Active low INT1
+    setMultiBitByIndex(&EICRA, 2, ISC01, 0, ISC00, 0);
+#if !defined(POLLING)
+    setBitByIndex(&EIMSK, INT0, 1);
+#endif
+    lis3dh_update_xyz();
+    sei();
+}
+
+/////// LIS3DH DATA READY INTERRUPT HANDLER ///////////////////////////////////
+
+volatile uint8_t int1_src = 0;
+volatile uint8_t status_reg = 0;
+
+ISR(INT0_vect) {
+    lis3dh_update_xyz();
+    int1_src = lis3dh_read(INT1_SOURCE);
+    status_reg = lis3dh_read(STATUS_REG2);
+}
+
+/////// 1 HZ PRINTING /////////////////////////////////////////////////////////
+
+volatile uint8_t overflows = 0;
+
+#define DIVIDER (256)
+#define INTERVAL ((uint8_t)((F_CPU / DIVIDER) / (1 << 8)))
+
+ISR(TIMER0_OVF_vect) {
+    ++overflows;
+    if (overflows >= INTERVAL) {
+#if defined(POLLING)
+        lis3dh_update_xyz();
+#endif
+        printf("XYZ: (%+d, %+d, %+d)\n", x, y, z);
+#if !defined(POLLING)
+        //printf("INT1_SOURCE: 0x%02x\n", int1_src);
+        //printf("STATUS_REG2: 0x%02x\n", status_reg);
+#endif
+        overflows = 0;
+    }
 }
 
 /////// MAIN //////////////////////////////////////////////////////////////////
@@ -156,10 +196,11 @@ ISR(INT0_vect) {
 int main(void) {
     cli();
     uart_init();
-    spi_init();
-    lis3dh_setup();
     setBitByIndex(&DDRC, DDC0, 1);
     setBitByIndex(&PORTC, PORTC0, 0);
-    sei();
+    setMultiBitByIndex(&TCCR0B, 3, CS02, 1, CS01, 0, CS00, 0);
+    setBitByIndex(&TIMSK0, TOIE0, 1);
+    spi_init();
+    lis3dh_setup();
     while (1) { }
 }
